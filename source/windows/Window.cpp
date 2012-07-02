a248eedce48a369ffd8adab7162146607e4f1bff
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2012-07-01 23:59:06 -0600

    oooh pretty colors

--- source/windows/Window.cpp
+++ source/windows/Window.cpp
@@ -23,6 +23,7 @@ namespace XPG
     static const DWORD BaseExStyle = WS_EX_APPWINDOW;
 
     static size_t openWindowCount = 0;
+    static const Window* activeWindow = NULL;
 
     LRESULT CALLBACK WindowCallback(HWND window, UINT message, WPARAM wparam,
         LPARAM lparam)
@@ -32,6 +33,12 @@ namespace XPG
 
         assert(meta != NULL);
 
+        if (activeWindow != meta->object)
+        {
+            activeWindow = meta->object;
+            wglMakeCurrent(meta->deviceContext, meta->renderContext);
+        }
+
         switch (message)
         {
             case WM_CLOSE:
@@ -40,6 +47,8 @@ namespace XPG
 
             case WM_PAINT:
                 cout << "WM_PAINT" << endl;
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+                meta->object->SwapBuffers();
                 break;
         }
 
@@ -64,6 +73,61 @@ namespace XPG
         }
 
         return result;
+    }
+
+    void SetupContext(WindowMeta* meta)
+    {
+        PIXELFORMATDESCRIPTOR pfd;
+        memset(&pfd, 0, sizeof(PIXELFORMATDESCRIPTOR));
+        pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+        pfd.dwFlags = PFD_DOUBLEBUFFER | PFD_SUPPORT_OPENGL |
+            PFD_DRAW_TO_WINDOW;
+        pfd.iPixelType = PFD_TYPE_RGBA;
+        pfd.cColorBits = 32;
+        pfd.cDepthBits = 32;
+        pfd.iLayerType = PFD_MAIN_PLANE;
+
+        int nPixelFormat = ChoosePixelFormat(meta->deviceContext, &pfd);
+        if (nPixelFormat == 0)
+        {
+            std::cerr << "failed ChoosePixelFormat\n";
+            return;
+        }
+
+        if (!SetPixelFormat(meta->deviceContext, nPixelFormat, &pfd))
+        {
+            std::cerr << "failed SetPixelFormat\n";
+            return;
+        }
+
+        meta->renderContext = wglCreateContext(meta->deviceContext);
+        wglMakeCurrent(meta->deviceContext, meta->renderContext);
+        activeWindow = meta->object;
+
+        GLenum e = glewInit();
+
+        if (e == GLEW_OK)
+        {
+            int attributes[] = {
+                WGL_CONTEXT_MAJOR_VERSION_ARB, 3,
+                WGL_CONTEXT_MINOR_VERSION_ARB, 1,
+                0, 0
+                };
+
+            if (wglewIsSupported("WGL_ARB_create_context") == 1)
+            {
+                HGLRC rc = wglCreateContextAttribsARB(meta->deviceContext,
+                    NULL, attributes);
+
+                if (rc)
+                {
+                    wglMakeCurrent(NULL, NULL);
+                    wglDeleteContext(meta->renderContext);
+                    meta->renderContext = rc;
+                    wglMakeCurrent(meta->deviceContext, meta->renderContext);
+                }
+            }
+        }
     }
 
     Window::Window()
@@ -105,6 +169,13 @@ namespace XPG
         }
 
         meta->deviceContext = GetDC(meta->window);
+
+        SetupContext(meta);
+
+        glViewport(0, 0, 640, 480);
+        glClearColor(openWindowCount, 0.5f, 0.5f, 1.0f);
+
+
         ShowWindow(meta->window, SW_SHOWNORMAL);
         UpdateWindow(meta->window);
 
@@ -114,6 +185,14 @@ namespace XPG
     void Window::Close()
     {
         WindowMeta* meta = (WindowMeta*)_native;
+
+        if (meta->renderContext)
+        {
+            wglMakeCurrent(meta->deviceContext, 0);
+            wglDeleteContext(meta->renderContext);
+
+            if (activeWindow == this) activeWindow = NULL;
+        }
 
         if (meta->deviceContext)
         {
