3dd942ff17f07f69be83c24ec0c4436d2b8a2968
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2013-01-06 01:13:41 +0000

    fixed TcpServer

--- samples/TcpServer/main.cpp
+++ samples/TcpServer/main.cpp
@@ -110,33 +110,8 @@ void SuperSimpleWebServer()
     }
 }
 
-void OnJoyAxis(XPG::UInt32 whichJoystick, XPG::UInt32 whichAxis, const XPG::Joystick& joyState)
-{
-    cerr << "Joystick axis " << whichAxis << endl;
-}
-
 int main(int argc, char** argv)
 {
-    unsigned int numJoysticks = XPG::Joystick::NumJoysticks();
-    cerr << "Num joysticks: " << numJoysticks << endl;
-
-//    for (unsigned int i = 0; i < numJoysticks; ++i)
-//    {
-//        XPG::Joystick joy(i);
-//        joy.PollState();
-//    }
-
-//    XPG::Joystick joy(2);
-
-    XPG::JoystickManager joyManager;
-    joyManager.OnJoyAxis(OnJoyAxis);
-
-    while(true)
-    {
-        joyManager.PollJoystickEvents();
-        XPG::Sleep(XPG::TimeSpan::FromMilliseconds(0));
-    }
-
     if (XPG::OpenSockets())
     {
         SuperSimpleWebServer();
